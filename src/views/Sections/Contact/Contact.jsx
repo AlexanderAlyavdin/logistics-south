e087ec8b722ec1e53f649631593dda785a37dad4
Alexander Alyavdin <alexander.alyavdin@gmail.com>
2022-02-21 18:00:33 +0300

    Februrary update

--- src/views/Sections/Contact/Contact.jsx
+++ src/views/Sections/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import { useStaticQuery, graphql } from "gatsby";
-import { useBreakpoint } from 'gatsby-plugin-breakpoints';
+import { useBreakpoint } from "gatsby-plugin-breakpoints";
 
 import { Row, Col } from "react-bootstrap";
 import Icon from "components/Icon";
@@ -10,7 +10,6 @@ import RussianMap from "components/Map/RussianMap";
 import CityContact from "components/Map/CityContact";
 import PageSection from "components/PageSection";
 import SectionHeader from "components/SectionHeader";
-
 
 const Contact = ({ className }) => {
   const breakpoint = useBreakpoint();
@@ -23,6 +22,7 @@ const Contact = ({ className }) => {
           header
           subheader
           contractFile
+          sheetFile
           telephone
           email
         }
@@ -30,24 +30,35 @@ const Contact = ({ className }) => {
     }
   `);
 
-  const [ selectedCity, setSelectedCity ] = useState('Москва');
+  const [selectedCity, setSelectedCity] = useState("Москва");
 
   const frontmatter = markdownRemark.frontmatter;
   if (!frontmatter) {
     return null;
   }
 
-  const { anchor, header, subheader, telephone, email, contractFile } = frontmatter;
+  const { anchor, header, subheader, telephone, email, sheetFile, contractFile } = frontmatter;
 
   return (
     <PageSection className={className} id={anchor}>
-      <SectionHeader header="Контакты" subheader=" "/>
+      <SectionHeader header="Контакты" subheader=" " />
 
-      <CityContact selectedCityName={selectedCity} onCitySelected={(city) => { setSelectedCity(city) }}/>
-      {!breakpoint.sm && <RussianMap selectedCity={selectedCity} onCitySelected={(city) => { setSelectedCity(city) }}/>}
+      <CityContact
+        selectedCityName={selectedCity}
+        onCitySelected={(city) => {
+          setSelectedCity(city);
+        }}
+      />
+      {!breakpoint.sm && (
+        <RussianMap
+          selectedCity={selectedCity}
+          onCitySelected={(city) => {
+            setSelectedCity(city);
+          }}
+        />
+      )}
 
-
-      <Row className={`justify-content-center ${breakpoint.sm ? 'mt-5' : ''}`}>
+      <Row className={`justify-content-center ${breakpoint.sm ? "mt-5" : ""}`}>
         <Col lg={8} className="text-center">
           <h2 className="mt-0">{header}</h2>
           <hr className="divider my-4" />
@@ -55,28 +66,34 @@ const Contact = ({ className }) => {
         </Col>
       </Row>
       <Row>
-        <Col lg={3} className="ml-auto text-center">
+        <Col lg={2} className="ml-auto text-center">
           <Icon iconName="PhoneIcon" size="3x" className="text-muted mb-3" />
           <a className="d-block" href={`tel:${telephone}`}>
             {telephone}
           </a>
         </Col>
-        <Col lg={3} className="text-center">
+        <Col lg={2} className="text-center">
           <Icon iconName="EnvelopIcon" size="3x" className="text-muted mb-3" />
           <a className="d-block" href={`mailto:${email}`}>
             {email}
           </a>
         </Col>
-        <Col lg={3} className="text-center">
+        <Col lg={2} className="text-center">
           <Icon iconName="MapMarkedAltIcon" size="3x" className="text-muted mb-3" />
           <a className="d-block" href="https://yandex.ru/maps/-/CCUA6WdjhA">
-            129110, г. Москва, ул.Щепкина, дом 58, строение 3, 3 этаж, помещение I, комната №3
+            107031, г. Москва, ул. Кузнецкий Мост, дом 21/5, этаж 5, помещение V, комната 30
           </a>
         </Col>
-        <Col lg={3} className="mr-auto text-center">
+        <Col lg={2} className="text-center">
           <Icon iconName="FileDownloadIcon" size="3x" className="text-muted mb-3" />
-          <a className="d-block" href={contractFile}>
+          <a className="d-block" href={contractFile} rel="noopener noreferrer" target="_blank">
             Договор поставки товара
+          </a>
+        </Col>
+        <Col lg={2} className="mr-auto text-center">
+          <Icon iconName="FileDownloadIcon" size="3x" className="text-muted mb-3" />
+          <a className="d-block" href={sheetFile} rel="noopener noreferrer" target="_blank">
+            Специальная оценка условий труда
           </a>
         </Col>
       </Row>
